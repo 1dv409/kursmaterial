a1104cbc9c94f096b6e0b90a152ce349e40b9d95
Mats Loock <mats.loock@lnu.se>
2013-11-08 12:26:09 +0100

    Uppdatering.

---
+++ Presentationsmaterial/Projekt/Hello/Hello/Controllers/HelloController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web.Mvc;
+using Hello.Models;
+
+namespace Hello.Controllers
+{
+    public class HelloController : Controller
+    {
+        public string Greet()
+        {
+            return "Hello, world!";
+        }
+
+        public ActionResult GreetWithView()
+        {
+            return View();
+        }
+
+        public ActionResult GreetWithViewBag()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ?
+                "Good morning" : "Good afternoon";
+            return View();
+        }
+
+        public ActionResult GreetWithViewBagAndModel()
+        {
+            ViewBag.Salutation = new Salutation();
+            return View();
+        }
+
+        public ActionResult GreetWithModel()
+        {
+            var model = new Salutation();
+            return View(model);
+        }
+        public ActionResult GreetWithModel2()
+        {
+            var model = new Salutation();
+            return View(model);
+        }
+    }
+}
---
+++ Presentationsmaterial/Projekt/Hello/Hello/Models/Salutation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hello.Models
+{
+    public class Salutation
+    {
+        public string Greeting
+        {
+            get
+            {
+                return DateTime.Now.Hour < 12 ?
+                    "Good morning" : "Good afternoon";
+            }
+        }
+    }
+}
