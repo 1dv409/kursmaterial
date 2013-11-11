b31a872ab539ca1c8b31acf8785ddcb8283dce8f
Mats Loock <mats.loock@lnu.se>
2013-11-11 09:32:46 +0100

    Uppdatering.

---
+++ Exempel/GeekGuestbook.v1/GeekGuestbook/Controllers/GuestbookController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GeekGuestbook.Models;
+using GeekGuestbook.Models.DataModels;
+
+namespace GeekGuestbook.Controllers
+{
+    public class GuestbookController : Controller
+    {
+        GeekGuestbookEntities _entities = new GeekGuestbookEntities();
+
+        //
+        // GET: /Guestbook/
+
+        public ActionResult Index()
+        {
+            var model = _entities.Messages.ToList();
+
+            return View("Index", model);
+        }
+
+        public ActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Message message)
+        {
+            message.Created = DateTime.Now;
+
+            _entities.Messages.Add(message);
+            _entities.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var message = _entities.Messages.Find(id);
+            return View("Delete", message);
+        }
+
+        [ActionName("Delete")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var message = _entities.Messages.Find(id);
+
+            _entities.Messages.Remove(message);
+            _entities.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Exempel/GeekGuestbook.v1/GeekGuestbook/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GeekGuestbook")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("LNU")]
+[assembly: AssemblyProduct("GeekGuestbook")]
+[assembly: AssemblyCopyright("Copyright © LNU 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d012d9d7-abf2-4259-87e0-6cd6d583f2d0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
