bbafededd0bad612c088a2e40306999768f83275
Mats Loock <mats.loock@lnu.se>
2013-11-24 10:13:35 +0100

    Uppdatering.

--- Exempel/HelloAjax/HelloAjax/App_Start/RouteConfig.cs
+++ Exempel/HelloAjax/HelloAjax/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace HelloAjax
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Hello", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Exempel/HelloAjax/HelloAjax/Controllers/HelloController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloAjax.Controllers
+{
+    public class HelloController : Controller
+    {
+        //
+        // GET: /Hello/
+
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(string greeting)
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("_Greeting", greeting);
+            }
+
+            ViewData.Model = greeting;
+            return View("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public string HelloAjax(string greeting)
+        {
+            return "Din Ajax-hälsning: " + greeting;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult HelloAjax2(string greeting)
+        {
+            if (Request.IsAjaxRequest())
+            {
+                return Content("Din Ajax-hälsning: " + greeting);
+            }
+
+            ViewData.Model = greeting;
+            return View("Index");
+        }
+
+    }
+}
