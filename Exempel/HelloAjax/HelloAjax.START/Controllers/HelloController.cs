f99ced7b46c193ed6ab0119a5a2d6effa49f3ef7
Mats Loock <mats.loock@lnu.se>
2013-11-25 08:37:53 +0100

    Uppdatering.

---
+++ Exempel/HelloAjax/HelloAjax.START/Controllers/HelloController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloAjax.Controllers
+{
+    public class HelloController : Controller
+    {
+        //
+        // GET: /Hello/
+
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        //
+        // POST: /Hello/
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(string greeting)
+        {
+            ViewData.Model = greeting;
+            return View("Index");
+        }
+
+    }
+}
