2b207329e57c417cc3ebda6f75e97a57ae1f9548
Mats Loock <mats.loock@lnu.se>
2014-11-04 14:28:07 +0100

    Uppdatering av GeekGuestbook (VS2013).

---
+++ Exempel/GeekGuestbook.MVC5/GeekGuestbook/Controllers/GuestbookController.cs
@@ -0,0 +1,47 @@
+﻿using GeekGuestbook.Models.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeekGuestbook.Controllers
+{
+    public class GuestbookController : Controller
+    {
+        private GeekGuestbookEntities _entities = new GeekGuestbookEntities();
+
+        // GET: Guestbook
+        public ActionResult Index()
+        {
+            var model = _entities.Messages.ToList();
+
+            return View(model);
+        }
+
+        // GET: Guestbook/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Guestbook/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Message message)
+        {
+            message.Created = DateTime.Now;
+
+            _entities.Messages.Add(message);
+            _entities.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _entities.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
