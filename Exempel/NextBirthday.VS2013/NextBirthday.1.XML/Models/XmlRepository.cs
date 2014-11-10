e0814e580d38733ffdd81fb9cf705ba397f9f0bd
Mats Loock <mats.loock@lnu.se>
2014-11-10 14:34:57 +0100

    NextBirthday XML & EF

---
+++ Exempel/NextBirthday.VS2013/NextBirthday.1.XML/Models/XmlRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace NextBirthdayXML.Models
+{
+    public class XmlRepository
+    {
+        private static readonly string PhysicalPath;
+
+        private XDocument _document;
+
+        private XDocument Document
+        {
+            get
+            {
+                return _document ?? (_document = XDocument.Load(PhysicalPath));
+            }
+        }
+
+        static XmlRepository()
+        {
+            PhysicalPath = Path.Combine(
+                AppDomain.CurrentDomain.GetData("DataDirectory").ToString(),
+                "Birthdates.xml");
+            //PhysicalPath = HttpContext.Current.Server.MapPath("~/App_Data/Birthdates.xml");
+        }
+
+        public List<Birthday> GetBirthdays()
+        {
+            return (from birthdate in Document.Descendants("birthdate")
+                    select new Birthday
+                    {
+                        Name = birthdate.Element("name").Value,
+                        Birthdate = DateTime.Parse(birthdate.Element("date").Value)
+                    }).OrderBy(b => b.DaysUntilNextBirthday).ToList();
+        }
+
+        public void InsertBirthday(Birthday birthday)
+        {
+            Document.Root.Add(
+                new XElement("birthdate",
+                    new XElement("name", birthday.Name),
+                    new XElement("date", birthday.Birthdate)));
+        }
+
+        public void Save()
+        {
+            Document.Save(PhysicalPath);
+        }
+    }
+}
---
+++ Exempel/NextBirthday.VS2013/NextBirthdayEF/Controllers/BirthdayController.cs
@@ -0,0 +1,62 @@
+﻿using NextBirthdayEF.Models;
+using NextBirthdayEF.Models.Repositories;
+using System.Web.Mvc;
+
+namespace NextBirthdayEF.Controllers
+{
+    public class BirthdayController : Controller
+    {
+        private IRepository _repository;
+
+        public BirthdayController()
+            : this(new EFRepository())
+        {
+            // Empty!
+        }
+
+        public BirthdayController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _repository.Dispose();
+            base.Dispose(disposing);
+        }
+
+        //
+        // GET: /Birthday/
+
+        public ActionResult Index()
+        {
+            return View("Index", _repository.GetBirthdays());
+        }
+
+        //
+        // GET: /Birthday/Create
+
+        public ActionResult Create()
+        {
+            return View("Create");
+        }
+
+        //
+        // POST: /Birthday/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Birthday birthday)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.InsertBirthday(birthday);
+                _repository.Save();
+
+                return RedirectToAction("Index");
+            }
+
+            return View("Create");
+        }
+    }
+}
