730b181b32f6de5d47b36bec8b3d8bdf2bf8f1b0
Mats Loock <mats.loock@lnu.se>
2014-11-10 14:36:11 +0100

    NextBirthday XML & EF

---
+++ Exempel/NextBirthday.VS2013/NextBirthday.1.XML/Controllers/BirthdayController.cs
@@ -0,0 +1,44 @@
+﻿using NextBirthdayXML.Models;
+using System.Web.Mvc;
+
+namespace NextBirthdayXML.Controllers
+{
+    public class BirthdayController : Controller
+    {
+        private XmlRepository _repository = new XmlRepository();
+
+        //
+        // GET: /Birthday/
+
+        public ActionResult Index()
+        {
+            return View("Index", _repository.GetBirthdays());
+        }
+
+        //
+        // GET: /Birthday/Create
+
+        public ActionResult Create()
+        {
+            return View("Create");
+        }
+
+        //
+        // POST: /Birthday/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Birthday birthday)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.InsertBirthday(birthday);
+                _repository.Save();
+
+                return RedirectToAction("Index");
+            }
+
+            return View("Create");
+        }
+    }
+}
