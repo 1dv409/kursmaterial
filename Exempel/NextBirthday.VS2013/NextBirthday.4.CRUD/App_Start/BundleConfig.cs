a0f2ad89b82b91b7bf080ca6a11c32e81a9fcf69
Mats Loock <mats.loock@lnu.se>
2014-11-16 17:58:32 +0100

    Fix validering klient

--- Exempel/NextBirthday.VS2013/NextBirthday.4.CRUD/App_Start/BundleConfig.cs
+++ Exempel/NextBirthday.VS2013/NextBirthday.4.CRUD/App_Start/BundleConfig.cs
@@ -12,6 +12,7 @@ namespace NextBirthdayCRUD
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/validatable.beforetoday.js",
                         "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
---
+++ Exempel/NextBirthday.VS2013/NextBirthday.4.CRUD/Scripts/validatable.beforetoday.js
@@ -0,0 +1,15 @@
+﻿(function ($) {
+	$.validator.addMethod("isDateLaterThanToday", function (value, element, params) {
+		if (!/Invalid|NaN/.test(new Date(value))) {
+			return new Date(value) < new Date();
+		}
+		return isNaN(value);
+	}, '');
+
+	$.validator.unobtrusive.adapters.add("beforetoday", {}, function (options) {
+		options.rules['isDateLaterThanToday'] = true;
+		options.messages['isDateLaterThanToday'] = options.message;
+	});
+}(jQuery));
+
+
